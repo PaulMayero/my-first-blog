a96812c16e28f57237adfa58aacbe325052706f5
PaulMayero <pmmayero@gmail.com>
2018-07-19 15:09:44 +0300

    just added some new views and templates

--- girls/blog/urls.py
+++ girls/blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
 url(r'^$',views.post_list,name='post_list'),
-url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),#view to go to direct posts
+url(r'^post/new/$', views.post_new, name='post_new'),#view to making new blopg posts
+url('post/new/',views.post_new,name='post_new'),#view to making new blopg posts
+url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ] 
